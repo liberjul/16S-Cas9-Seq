f92a053ae03adb83bbd3cad0c5b0314e0baa2c64
liberjul <liberjul@msu.edu>
2022-05-04 11:10:27 -0400

    Update to CONSTAX script.

--- scripts/03a_classify_constax.sh
+++ scripts/03a_classify_constax.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 ########## Define Resources Needed with SBATCH Lines ##########
 
-#SBATCH --time=6:00:00             # limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --time=02:00:00             # limit of wall clock time - how long the job will run (same as -t)
 #SBATCH --ntasks=1                 # number of tasks - how many tasks (nodes) that you require (same as -n)
 #SBATCH --cpus-per-task=20          # number of CPUs (or cores) per task (same as -c)
 #SBATCH --mem=32G                   # memory required per node - amount of memory (in bytes)
@@ -19,13 +19,12 @@ do
   -n $SLURM_CPUS_PER_TASK \
   -d /mnt/ufs18/rs-022/bonito_lab/CONSTAX_May2020/SILVA_Bacteria_tf/SILVA_138_SSURef_tax_silva_bact.fasta \
   -f /mnt/ufs18/rs-022/bonito_lab/CONSTAX_May2020/SILVA_Bacteria_tf --mem $SLURM_MEM_PER_NODE -m 20 \
-  -x ./taxonomy/tax_bact \
-  -o ./taxonomy/out_bact \
+  -x ./taxonomy/tax_bact_$i \
+  -o ./taxonomy/out_bact_$i \
   --high_level_db /mnt/ufs18/home-026/liberjul/Bonito_Lab/constax/test_new/SILVA138_NR_blastdb/SILVA138_NRdb.fasta \
   --conservative \
   --consistent \
   --msu_hpcc
-  mv ./taxonomy/out_bact/constax_taxonomy.txt ./out_bact/constax_taxonomy_$i.txt
 done
 
 scontrol show job $SLURM_JOB_ID     ### write job information to output file
